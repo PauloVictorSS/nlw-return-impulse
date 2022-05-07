6168476cc9280181472ffd207a0ac6e594230efc
Paulo Victor <p248438@dac.unicamp.br>
2022-05-07 12:48:08 -0300

    Finalização do Front-End da aplicação web

---
+++ src/components/Loading.tsx
@@ -0,0 +1,9 @@
+import { CircleNotch } from "phosphor-react";
+
+export function Loading(){
+    return (
+        <div className="w6 h-6 flex items-center justify-center overflow-hidden">
+            <CircleNotch weight="bold" className="h-4 w-4 animate-spin"/>
+        </div>
+    )
+}
